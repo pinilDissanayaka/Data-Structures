b9d2b9f019b7c0a8cd363ede3ef636a5ac86f53b
pinilDissanayaka <pinildissanayaka@gmail.com>
2023-12-17 13:20:32 +0530

    Add files via upload

---
+++ DSA/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char arr[] = {'{', '(', '{', '}', ')', '}'};
+
+        BrMatch b = new BrMatch();
+        b.match(arr);
+    }
+}
+
